691372389a3928bbc992bd6573ca1adfeef1cf8e
Kyle Zaragoza <popwarsweet@gmail.com>
2015-09-11 16:34:42 -0700

    fix for blank frames at beginning of video

--- framework/Source/iOS/GPUImageMovieWriter.m
+++ framework/Source/iOS/GPUImageMovieWriter.m
@@ -264,6 +264,7 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
     }
 }
 
+static BOOL allowWriteAudio = NO;
 - (void)startRecording;
 {
     alreadyFinishedRecording = NO;
@@ -276,6 +277,7 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
     });
     isRecording = YES;
 	//    [assetWriter startSessionAtSourceTime:kCMTimeZero];
+    allowWriteAudio = NO;
 }
 
 - (void)startRecordingInOrientation:(CGAffineTransform)orientationTransform;
@@ -362,6 +364,10 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
 
 - (void)processAudioBuffer:(CMSampleBufferRef)audioBuffer;
 {
+    if (!allowWriteAudio) {
+        return;
+    }
+    
     if (!isRecording)
     {
         return;
@@ -746,6 +752,8 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
             {
                 if (![assetWriterPixelBufferInput appendPixelBuffer:pixel_buffer withPresentationTime:frameTime])
                     NSLog(@"Problem appending pixel buffer at time: %@", CFBridgingRelease(CMTimeCopyDescription(kCFAllocatorDefault, frameTime)));
+                
+                allowWriteAudio = YES;
             }
             else
             {
